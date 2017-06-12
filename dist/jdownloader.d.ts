644c70a6d2a89c14497d0ef98d254c71d31309c6
Ilan Frumer <ilanfrumer@gmail.com>
2017-06-12 18:00:22 +0300

    versions

--- dist/jdownloader.d.ts
+++
@@ -1,6 +0,0 @@
-export interface IJDownloaderOptions {
-    uri: string | string[];
-    pattern?: string;
-    packageName?: string;
-}
-export declare function jdownloader({uri, pattern, packageName}: IJDownloaderOptions): Promise<number>;
