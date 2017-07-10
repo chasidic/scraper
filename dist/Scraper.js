ed0c73f18f07daaf7c4d23424c717b56fddccaf6
Ilan Frumer <ilanfrumer@gmail.com>
2017-07-10 17:20:23 +0300

    versions

--- dist/Scraper.js
+++ dist/Scraper.js
@@ -1,6 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const tslib_1 = require("tslib");
 const cache_1 = require("@chasidic/cache");
 const xmldom_1 = require("xmldom");
 const cheerio_1 = require("cheerio");
@@ -11,53 +10,43 @@ class Scraper {
         this._sleep = sleep;
         this._retries = retries;
     }
-    fetch(uri) {
-        return tslib_1.__awaiter(this, void 0, void 0, function* () {
-            if (!(yield this._cache.has(uri))) {
-                let retries = 1;
-                let error = null;
-                do {
-                    try {
-                        let cache = yield lib_1.requestAsync(uri);
-                        yield this._cache.set(uri, cache);
-                        return;
-                    }
-                    catch (e) {
-                        error = e;
-                        yield lib_1.sleep(this._sleep);
-                    }
-                } while (++retries < this._retries);
-                throw error;
-            }
-        });
+    async fetch(uri) {
+        if (!await this._cache.has(uri)) {
+            let retries = 1;
+            let error = null;
+            do {
+                try {
+                    let cache = await lib_1.requestAsync(uri);
+                    await this._cache.set(uri, cache);
+                    return;
+                }
+                catch (e) {
+                    error = e;
+                    await lib_1.sleep(this._sleep);
+                }
+            } while (++retries < this._retries);
+            throw error;
+        }
     }
-    load(uri) {
-        return tslib_1.__awaiter(this, void 0, void 0, function* () {
-            yield this.fetch(uri);
-            return yield this._cache.get(uri);
-        });
+    async load(uri) {
+        await this.fetch(uri);
+        return await this._cache.get(uri);
     }
-    loadDOM(uri, options = {}) {
-        return tslib_1.__awaiter(this, void 0, void 0, function* () {
-            const body = yield this.load(uri);
-            const parser = new xmldom_1.DOMParser(options);
-            return parser.parseFromString(body, 'text/xml');
-        });
+    async loadDOM(uri, options = {}) {
+        const body = await this.load(uri);
+        const parser = new xmldom_1.DOMParser(options);
+        return parser.parseFromString(body, 'text/xml');
     }
-    loadCheerio(uri) {
-        return tslib_1.__awaiter(this, void 0, void 0, function* () {
-            const body = yield this.load(uri);
-            const normalizeWhitespace = true;
-            const decodeEntities = false;
-            const xmlMode = lib_1.isXMLFilename(uri);
-            return cheerio_1.load(body, { normalizeWhitespace, decodeEntities, xmlMode });
-        });
+    async loadCheerio(uri) {
+        const body = await this.load(uri);
+        const normalizeWhitespace = true;
+        const decodeEntities = false;
+        const xmlMode = lib_1.isXMLFilename(uri);
+        return cheerio_1.load(body, { normalizeWhitespace, decodeEntities, xmlMode });
     }
-    tree(uri, indent = 2) {
-        return tslib_1.__awaiter(this, void 0, void 0, function* () {
-            let $ = yield this.loadCheerio(uri);
-            return lib_1.toJade($, indent);
-        });
+    async tree(uri, indent = 2) {
+        let $ = await this.loadCheerio(uri);
+        return lib_1.toJade($, indent);
     }
 }
 exports.Scraper = Scraper;
--- dist/lib/requestAsync.js
+++ dist/lib/requestAsync.js
@@ -1,30 +1,27 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const tslib_1 = require("tslib");
 const request_1 = require("request");
 const isXMLFilename_1 = require("./isXMLFilename");
 const bufferToString_1 = require("./bufferToString");
 const REPLACEMENT_CHAR_REGEX = /\uFFFD/;
-function requestAsync(uri) {
-    return tslib_1.__awaiter(this, void 0, void 0, function* () {
-        return new Promise((resolve, reject) => {
-            request_1.get(uri, { encoding: null }, (err, res, buffer) => {
-                if (err) {
-                    reject(err.message);
-                }
-                else if (res.statusCode !== 200) {
-                    reject(res.statusCode);
+async function requestAsync(uri) {
+    return new Promise((resolve, reject) => {
+        request_1.get(uri, { encoding: null }, (err, res, buffer) => {
+            if (err) {
+                reject(err.message);
+            }
+            else if (res.statusCode !== 200) {
+                reject(res.statusCode);
+            }
+            else {
+                let cache = bufferToString_1.bufferToString(buffer, isXMLFilename_1.isXMLFilename(uri));
+                if (REPLACEMENT_CHAR_REGEX.test(cache)) {
+                    reject('corrupted body');
                 }
                 else {
-                    let cache = bufferToString_1.bufferToString(buffer, isXMLFilename_1.isXMLFilename(uri));
-                    if (REPLACEMENT_CHAR_REGEX.test(cache)) {
-                        reject('corrupted body');
-                    }
-                    else {
-                        resolve(cache);
-                    }
+                    resolve(cache);
                 }
-            });
+            }
         });
     });
 }
