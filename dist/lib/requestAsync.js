85b4f8b76ca86f6f96705688e5a0d19554101f11
Ilan Frumer <ilanfrumer@gmail.com>
2017-11-26 07:14:39 +0200

    upgrade

--- dist/lib/requestAsync.js
+++ dist/lib/requestAsync.js
@@ -4,9 +4,12 @@ const request_1 = require("request");
 const isXMLFilename_1 = require("./isXMLFilename");
 const bufferToString_1 = require("./bufferToString");
 const REPLACEMENT_CHAR_REGEX = /\uFFFD/;
+const headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
+};
 async function requestAsync(uri) {
     return new Promise((resolve, reject) => {
-        request_1.get(uri, { encoding: null }, (err, res, buffer) => {
+        request_1.get(uri, { encoding: null, headers }, (err, res, buffer) => {
             if (err) {
                 reject(err.message);
             }
--- src/lib/requestAsync.ts
+++ src/lib/requestAsync.ts
@@ -3,10 +3,13 @@ import { isXMLFilename } from './isXMLFilename';
 import { bufferToString } from './bufferToString';
 
 const REPLACEMENT_CHAR_REGEX = /\uFFFD/;
+const headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
+};
 
 export async function requestAsync(uri: string) {
     return new Promise<string>((resolve, reject) => {
-        get(uri, { encoding: null }, (err, res, buffer) => {
+        get(uri, { encoding: null, headers }, (err, res, buffer) => {
             if (err) {
                 reject(err.message);
             } else if (res.statusCode !== 200) {
